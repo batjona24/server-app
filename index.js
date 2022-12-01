5c542edbdcd735885187ac169a958531c8c41dff
batjona24 <88026430+batjona24@users.noreply.github.com>
2022-12-01 15:52:36 +0100

    create

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.post('/set', async (request, response) => {
+    const body = request.body;
+    await fsp.writeFile("data.json", body);
+    response.status(200);
+    response.json(body);
+});
+
+app.get('/get', async (request, response) => {
+    const content = await fsp.readFile("./data.json","utf-8"); 
+    response.status(200);
+    response.json(content);
+});
+
+app.delete('/remove', async (request, response) => {
+    await fsp.unlink("data.json");
+    response.status(200);
+    response.json(true);
+});
+
+app.all('/*', async (request, response) => {
+    response.status(404);
+    response.json({ error: 'I can only give you authors, this route does not exist' });
+});
+
+const hostname = 'localhost';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server listening on http://${hostname}:${port}`)
+});
