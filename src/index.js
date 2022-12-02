6943a4adefaa5df9b834118c9de289d7fe13ea2d
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-12-02 17:55:49 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ app.use(express.json());
 
 app.post('/trip', async (request, response) => {
     const data_trip = request.body;
-    const insert_result = await database.raw(`insert into trips (date , vacation, days, rating )
-    values ('${data_trip.date}, ${data_trip.vacation},${data_trip.days},${data_trip.rating}')`)
+    await database.raw(`insert into trips (date , vacation, days, rating )
+    values ('${data_trip.date}',' ${data_trip.vacation}',${data_trip.days},${data_trip.rating})`);
+    const result = await database.raw(`select * from trips`);
     response.status(200);
-    response.json(insert_result);
+    response.json(result);
 });
 
 app.get('/trip/:id', async (request, response) => {
     const id = request.params.id;
     const result = await database.raw(`select * from trips where id = ${id}`);
-    response.status(200);
-    response.json(result);
+    //this does not work, still returns 200 and an empty array
+    if(result !==[]) {
+      response.status(200);
+      response.json(result);  
+    }
+    else {
+        response.status(404);
+        response.json(`The trip with id: ${id} NOT FOUND!`);
+    }
+    
 });
 
 app.get('/trip', async (request, response) => {
@@ -26,8 +35,9 @@ app.get('/trip', async (request, response) => {
 
 app.put('/trip/:id', async (request, response) => {
     const id = request.params.id;
-    const result = await database.raw(`update trips (date , vacation, days, rating ) 
-    values ('${data_trip.date}, ${data_trip.vacation},${data_trip.days},${data_trip.rating} where id=${id} `);
+    const data_trip = request.body;
+    await database.raw(`update trips set date ='${data_trip.date}', vacation = '${data_trip.vacation}', days = ${data_trip.days}, rating = ${data_trip.rating} where id=${id} `);
+    const result = await database.raw(`select * from trips where id=${id}`);
     response.status(200)
     response.json(result)
    
@@ -36,8 +46,8 @@ app.put('/trip/:id', async (request, response) => {
 app.delete('/trip/:id', async (request, response) => {
     const id = request.params.id;
     const result = await database.raw(`delete from trips where id=${id}`);
-    response.status(200)
-    response.json(result)
+    response.status(200);
+    response.json(true);
 });
 
 app.all('/*', async (request, response) => {
